221c3b012856aee157ffd49242090a5de0af3ebb
Killerjoy1 <43672513+Killerjoy1@users.noreply.github.com>
2019-05-14 18:13:42 +0200

    Första fienden
    Animationer, Fienden följer spelaren, navmesh på "golvet"

---
+++ Slutprojekt - Top Down 2d Shooter/Assets/Mina Scripts/EnemyMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+	Transform player;
+	UnityEngine.AI.NavMeshAgent nav;
+
+	void Awake ()
+	{
+		  player = GameObject.Find ("Player").transform;
+
+
+			nav = GetComponent <UnityEngine.AI.NavMeshAgent>();
+	}
+
+
+	void Update ()
+	{
+			nav.SetDestination (player.position);
+	}
+}
