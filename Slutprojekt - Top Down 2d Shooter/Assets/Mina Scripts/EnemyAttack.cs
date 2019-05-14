74eaa1f27899977a797ae748621d0ef10cdb36ba
Killerjoy1 <43672513+Killerjoy1@users.noreply.github.com>
2019-05-14 21:52:39 +0200

    Kan nu döda fiender
    Fiender kan dö, spelarens vapen kan skjuta

--- Slutprojekt - Top Down 2d Shooter/Assets/Mina Scripts/EnemyAttack.cs
+++ Slutprojekt - Top Down 2d Shooter/Assets/Mina Scripts/EnemyAttack.cs
@@ -12,7 +12,7 @@ namespace CompleteProject
         Animator anim;                              // Reference to the animator component.
         GameObject player;                          // Reference to the player GameObject.
         PlayerHealth playerHealth;                  // Reference to the player's health.
-        //EnemyHealth enemyHealth;                    // Reference to this enemy's health.
+        EnemyHealth enemyHealth;                    // Reference to this enemy's health.
         bool playerInRange;                         // Whether player is within the trigger collider and can be attacked.
         float timer;                                // Timer for counting up to the next attack.
 
@@ -22,7 +22,7 @@ namespace CompleteProject
             // Setting up the references.
             player = GameObject.FindGameObjectWithTag ("Player");
             playerHealth = player.GetComponent <PlayerHealth> ();
-            //enemyHealth = GetComponent<EnemyHealth>();
+            enemyHealth = GetComponent<EnemyHealth>();
             anim = GetComponent <Animator> ();
         }
 
@@ -55,7 +55,7 @@ namespace CompleteProject
             timer += Time.deltaTime;
 
             // If the timer exceeds the time between attacks, the player is in range and this enemy is alive...
-            if(timer >= timeBetweenAttacks && playerInRange /* && enemyHealth.currentHealth > 0*/)
+            if(timer >= timeBetweenAttacks && playerInRange  && enemyHealth.currentHealth > 0)
             {
                 // ... attack.
                 Attack ();
--- Slutprojekt - Top Down 2d Shooter/Assets/Mina Scripts/EnemyMovement.cs
+++ Slutprojekt - Top Down 2d Shooter/Assets/Mina Scripts/EnemyMovement.cs
@@ -2,22 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+namespace CompleteProject
+{
 public class EnemyMovement : MonoBehaviour {
 
 	Transform player;
+	PlayerHealth playerHealth;
+	EnemyHealth enemyHealth;
 	UnityEngine.AI.NavMeshAgent nav;
 
 	void Awake ()
 	{
 		  player = GameObject.Find ("Player").transform;
-
-
+			playerHealth = player.GetComponent <PlayerHealth> ();
+			enemyHealth = GetComponent <EnemyHealth> ();
 			nav = GetComponent <UnityEngine.AI.NavMeshAgent>();
 	}
 
 
 	void Update ()
-	{
-			nav.SetDestination (player.position);
+		{
+			if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+			{
+					nav.SetDestination (player.position);
+			}
+			else
+			{
+					nav.enabled = false;
+			}
+		}
 	}
 }
